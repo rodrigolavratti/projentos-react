ac27007b52360dafcabb250530f0fb8996f8e4f5
rodrigolavratti <rodrigo.l.lavratti@gmail.com>
2023-04-18 11:12:21 -0300

    frontend

---
+++ 12_REACTGRAM/frontend/src/services/authService.js
@@ -0,0 +1,55 @@
+import { api, requestConfig } from '../utils/config';
+
+// Registrar usuário no sistema
+const register = async (data) => {
+
+    const config = requestConfig("POST", data);
+
+    try {
+        const res = await fetch(api + "/users/register", config)
+        .then((res) => res.json())
+        .catch((err) => err);
+
+        if(res) {
+            localStorage.setItem("user", JSON.stringify(res));
+        }
+        
+        return res;
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Logout an user | sair do sistema
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+// Login do usuário | Sing in an user
+const login = async (data) => {
+    const config = requestConfig("POST", data);
+
+    try {
+        const res = await fetch(api + "/users/login", config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        if(res) {
+            localStorage.setItem("user", JSON.stringify(res))
+        }
+
+        return res;
+
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+const authService = {
+    register,
+    logout,
+    login,
+}
+
+export default authService;
