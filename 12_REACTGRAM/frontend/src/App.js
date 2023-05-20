f59550e2403cd2ac2624e76462ecac20c7658ba5
rodrigolavratti <rodrigo.l.lavratti@gmail.com>
2023-05-20 16:18:28 -0300

    Rotas dos links

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Router
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
